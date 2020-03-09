958c2feda44fcf10d0fad5a201e86fa5b14e633e
123JackCole <123jack.cole@gmail.com>
2020-03-09 15:47:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, name) {
+    let returnArray = [];
+    for(let i = 0; i < array.length; i++) {
+        returnArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    return returnArray;
+}
+
+function countDown(positiveInteger) {
+    while(positiveInteger >= 0) {
+        console.log(positiveInteger)
+        positiveInteger--;
+    }
+}
