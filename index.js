05731c7cfb19147c4706a15ca8f640e302349c62
Agandaur <cjwagstaffe@gmail.com>
2020-03-09 15:08:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, message) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+      }
+      return array
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number -= 1  
+    }
+}
