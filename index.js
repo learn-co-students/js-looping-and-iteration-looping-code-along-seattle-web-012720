ebe9c6f80a21d9b562a25c531ba624270196abbf
github username <jonathanriecke@gmail.com>
2020-03-09 13:14:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(arr, holiday){
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${holiday} gift!`)
+    }
+    return newArr;
+}
+
+function countDown(int){
+    while (int >= 0) {
+        console.log(int--);
+    }
+};
